ab293db38926efac006103e6494e8a3992945f12
JeanAranda <jarandar2014@gmail.com>
2023-05-25 01:53:43 -0500

    Se subio los ambios de los metodos abstractos sin errrores en cuenta corriente

--- src/CuentaAhorros.java
+++ src/CuentaAhorros.java
@@ -1,8 +1,13 @@
 
 public class CuentaAhorros extends Cuenta {
-
+	
 	public CuentaAhorros(int agencia, int numero) {
 		super(agencia, numero);
+	}
+
+	@Override
+	public void deposita(double valor) {
+		this.saldo = this.saldo + valor;		
 	}
 	
 }
